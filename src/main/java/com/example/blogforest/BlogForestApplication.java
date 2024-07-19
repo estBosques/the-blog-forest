fea84d4ecac542be22b99ad0871614095676656c
Esteban Bosques <nano.95.eb@gmail.com>
2024-07-19 17:05:08 -0600

    first commit

---
+++ src/main/java/com/example/blogforest/BlogForestApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogforest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogForestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogForestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/blogforest/BlogForestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogforest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogForestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
